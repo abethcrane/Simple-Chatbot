7fdd2de2f5a8c9b74841517b78743f395d6399ac
beth <bethc@cse.unsw.edu.au>
2013-01-11 19:57:54 +1100

    Added in email functionality

--- chatbot.py
+++ chatbot.py
@@ -30,18 +30,40 @@ def EditSpreadsheet(items, email):
         data['email'] = email
         gs_client.InsertRow(data, spreadsheet_key, worksheet_id)
 
+def SendEmail(recipient, subject, body, sender):     
+    '''Sends an e-mail to the specified recipient.'''
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587
+
+    #body = "" + body + ""  # why?
+     
+    if sender is not "" and sender is not None:
+        from_addr = email + " via " + str(sender)
+    else:
+        from_addr = email
+        
+    print from_addr
+    headers = ["From: " + from_addr,
+               "Subject: " + subject,
+               "To: " + recipient,
+               "MIME-Version: 1.0",
+               "Content-Type: text/html"]
+    headers = "\r\n".join(headers)
+     
+    session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        
+    session.ehlo()
+    session.starttls()
+    session.ehlo
+    session.login(email, password)
+     
+    session.sendmail(email, recipient, headers + "\r\n\r\n" + body)
+    session.quit()
+
 #########################################################################################
 
 class SampleBot(GtalkRobot):
-    
-    #Regular Expression Pattern Tips:
-    # I or IGNORECASE <=> (?i)      case insensitive matching
-    # L or LOCALE <=> (?L)          make \w, \W, \b, \B dependent on the current locale
-    # M or MULTILINE <=> (?m)       matches every new line and not only start/end of the whole string
-    # S or DOTALL <=> (?s)          '.' matches ALL chars, including newline
-    # U or UNICODE <=> (?u)         Make \w, \W, \b, and \B dependent on the Unicode character properties database.
-    # X or VERBOSE <=> (?x)         Ignores whitespace outside character sets
-    
+
     #"command_" is the command prefix, "001" is the priviledge num, "setState" is the method name.
     #This method is used to change the state and status text of the bot.
     def command_001_setState(self, user, message, args):
@@ -57,6 +79,19 @@ class SampleBot(GtalkRobot):
             print jid, " ---> ",bot.getResources(jid), bot.getShow(jid), bot.getStatus(jid)
             self.setState(show, status)
             self.replyMessage(user, "State settings changed!")
+
+    #This method is used to send email for users.
+    def command_002_SendEmail(self, user, message, args):
+        #email blah@gmail.com "hello blah" "nice to meet you"
+        '''(email|mail|em|m)\s+(.*?@.+?)\s+(.*?),\s*(.*)(?i)'''
+        user_email = re.sub("\/.*", "", str(user))
+        email_addr = args[1]
+        subject = args[2]
+        body = args[3]
+        body = body + "\n\nSent from a Chatbot via " + user_email
+        SendEmail(email_addr, subject, body, user_email)
+        
+        self.replyMessage(user, "\nEmail sent to "+ email_addr +" at: "+time.strftime("%Y-%m-%d %a %H:%M:%S", time.gmtime()))
 
     def command_003_AddItem(self, user, message, args):
         '''add\((.*)\)'''
